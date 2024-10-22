58e1aeae92113592a8446862cceb7163ced0930b
Siyuan Jing <crazycyy233@gmail.com>
2024-10-21 23:05:19 -0400

    Add files via upload

---
+++ Store Inputs Codes/Task_1.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/10/2024 02:04:27 PM
+// Design Name: 
+// Module Name: Task_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module Task_1(    
+    input clk, rst,        
+    input [1:0] btn,       // For better control of simulation
+    input [9:0] switches,  // 10-bit binary input, 10 switches
+//    output [3:0] leds,     // Display N, V, C, Z
+//    output [6:0] seven_seg, // Display outputs to seven segment
+//    output [3:0] seven_enable,
+    output reg [9:0] sequence
+    // Select which seven segment to display
+);
+
+    initial begin
+        sequence = 10'b0; 
+    end
+
+    // Register update logic
+
+    always @( posedge clk) begin
+        if(rst)begin 
+           sequence <= switches;
+        end   
+    end
+
+endmodule
+
---
+++ Store Inputs Codes/task_1_testbench.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/10/2024 02:09:21 PM
+// Design Name: 
+// Module Name: task_1_testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module task_1_testbench;
+
+    reg [9:0] switches;
+    reg rst;
+    reg clk;
+    wire [9:0] sequence;
+    
+    Task_1 uut (
+        .switches(switches),
+        .rst(rst),
+        .clk(clk),
+        .sequence(sequence)
+    );
+    always #2 clk=~clk;
+    initial begin
+        // Initialize Inputs
+       
+        switches = 10'b0;    
+        rst = 1'b0;        
+        clk=1'b0;
+ 
+        #30;
+        switches[0] = 1;
+        switches[2] = 1;
+        switches[3] = 1;
+        switches[5] = 1;
+        switches[6] = 1;
+        switches[7] = 1;
+        switches[9] = 1;
+
+        #10;   
+        rst = 1;    
+        #5;
+        rst = 0; 
+           
+        #25    
+        switches[0] = 0;
+        switches[6] = 0;
+        switches[8] = 1;
+        switches[9] = 0;
+        
+        #100
+        rst = 1;    
+        #5;
+        rst = 0; 
+    end
+
+endmodule
+
