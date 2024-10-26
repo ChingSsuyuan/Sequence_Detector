4292b89f96448b3a47bdc6663f1a21c7523d3ae6
Siyuan Jing <crazycyy233@gmail.com>
2024-10-26 13:58:52 -0400

    Add files via upload

---
+++ Top Module/Top_Module.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+
+
+module Top_module(clk, rst, switches, seven_seg, seven_enable,leds);
+    input clk, rst; // Needed for synchronous inputs
+    input [10:0] switches; // Input switches
+    output [3:0] leds; 
+    output [6:0] seven_seg; // Display outputs to seven segment
+    output  [3:0] seven_enable; // Select which seven segment to display
+    wire [3:0] out_from_state_machine;
+    wire clk2;
+    ClockDivider CD (.clk(clk),.clk_out(clk2));
+
+     state_machine2 sm (
+        .clk(clk2),
+        .rst(rst),
+        .switches(switches),
+        .out(out_from_state_machine),
+        .led_out(leds)
+    );
+
+
+    
+    seven_segment sev_seg(.in(out_from_state_machine), .seven_enable(seven_enable[0]), .A(seven_seg[0]), .B(seven_seg[1]), .C(seven_seg[2]), .D(seven_seg[3]), .E(seven_seg[4]), .F(seven_seg[5]), .G(seven_seg[6]));
+
+
+endmodule
+
+
+
+
