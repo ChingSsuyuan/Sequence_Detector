a0661a81c414785c0a0f0e8355da63b3e43e6990
Siyuan Jing <crazycyy233@gmail.com>
2024-10-29 00:21:07 -0400

    Update and rename state_machine2.v to State_machine2.v

--- Top Module/State_machine2.v
+++ Top Module/State_machine2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/10/2024 09:44:20 PM
-// Design Name: 
-// Module Name: state_machine2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module state_machine2(
 
